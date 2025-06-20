09fe1863cc3d8d786f6af953ba4fcc9c9c9506d5
majid-the-dev <contactmajidoladepo@gmail.com>
2025-06-20 10:57:56 +0100

    Fix: Hero overlay

--- components/website/homepage/Hero.tsx
+++ components/website/homepage/Hero.tsx
@@ -5,26 +5,32 @@ import BG from "@/public/assets/homepage-hero-background.jpg";
 
 const Hero = () => {
   return (
-    <section className="relative h-[90vh] w-full bg-primary flex flex-col items-center justify-center overflow-x-hidden">
-      <div className="mx-auto w-full max-w-7xl px-5 md:px-10">
-        <Image src={BG} layout="fill" objectFit="cover" alt="Harmony BookMe" />
+    <section className="relative h-[90vh] w-full flex items-center justify-center overflow-hidden">
+      <div className="absolute inset-0 -z-10">
+        <Image
+          src={BG}
+          alt="Harmony BookMe"
+          fill
+          className="object-cover"
+          priority
+        />
         <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-primary/30"></div>
-        <div className="relative flex flex-col items-center justify-center">
-          <h1 className="mb-5 text-white text-center text-4xl font-bold md:text-7xl/[80px]">
-            Simplified Booking!
-          </h1>
-          <p className="text-center text-white/80 text-sm/relaxed md:text-base/relaxed font-semibold mx-auto max-w-3xl">
-            We&apos;ve built a platform where you can book everything—from
-            electrifying concerts and must-see movies to essential rentals and
-            unforgettable getaways.
-          </p>
-          <Link
-            href={"/"}
-            className="flex items-center gap-2 text-white/80 text-sm md:text-base font-semibold border rounded-full px-5 py-2 mt-10"
-          >
-            Discover
-          </Link>
-        </div>
+      </div>
+      <div className="relative z-10 mx-auto w-full max-w-7xl px-5 md:px-10 text-center">
+        <h1 className="mb-5 text-white text-4xl font-bold md:text-7xl/[80px]">
+          Simplified Booking!
+        </h1>
+        <p className="text-white/80 text-sm md:text-base font-semibold mx-auto max-w-3xl">
+          We&apos;ve built a platform where you can book everything—from
+          electrifying concerts and must-see movies to essential rentals and
+          unforgettable getaways.
+        </p>
+        <Link
+          href="/"
+          className="inline-flex items-center gap-2 text-white/80 text-sm md:text-base font-semibold border rounded-full px-5 py-2 mt-10"
+        >
+          Discover
+        </Link>
       </div>
     </section>
   );
