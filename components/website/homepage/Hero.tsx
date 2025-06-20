ed621db5907800c20d0b8bec3f5876bbfcca61f9
majid-the-dev <contactmajidoladepo@gmail.com>
2025-06-20 11:20:21 +0100

    Fix: Hero overlay

--- components/website/homepage/Hero.tsx
+++ components/website/homepage/Hero.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import BG from "@/public/assets/homepage-hero-background.jpg";
 
 const Hero = () => {
   return (
-    <section className="relative h-[90vh] w-full flex items-center justify-center overflow-hidden bg-black text-white">
-      {/* Background Image + Gradient Overlay using ::before */}
-      <div className="absolute inset-0 before:absolute before:inset-0 before:bg-gradient-to-t before:from-black/80 before:to-primary/30 before:z-10 bg-[url('/assets/homepage-hero-background.jpg')] bg-cover bg-center z-0"></div>
+    <section className="relative h-[90vh] w-full flex items-center justify-center overflow-hidden bg-black">
+      {/* Background wrapper with image and gradient */}
+      <div className="absolute inset-0 w-full h-full z-0 pointer-events-none">
+        <Image
+          src={BG}
+          alt="Harmony BookMe"
+          fill
+          className="object-cover blur-xs"
+          priority
+          sizes="100vw"
+        />
+        <div className="absolute inset-0 bg-gradient-to-t from-black/85 to-primary/70 z-10"></div>
+      </div>
 
       {/* Foreground content */}
       <div className="relative z-20 text-center max-w-4xl px-5">
-        <h1 className="mb-5 text-4xl font-bold md:text-7xl/[80px]">
+        <h1 className="mb-5 text-white text-4xl font-bold md:text-7xl/[80px]">
           Simplified Booking!
         </h1>
         <p className="text-white/80 text-sm md:text-base font-semibold mx-auto">
