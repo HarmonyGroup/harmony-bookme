3af463cd3b4db8f54bb710b8add712db3ee01df4
majid-the-dev <contactmajidoladepo@gmail.com>
2025-10-02 01:52:07 +0100

    fix: UI changes

--- app/vendor/events/page.tsx
+++ app/vendor/events/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Page = () => {
   return (
     <section className="h-full flex flex-col bg-muted/60 p-4 md:p-6 pt-8 md:pt-6">
-      <div className="flex items-start md:items-center justify-between gap-4">
+      <div className="flex items-start flex-col md:flex-row md:items-center justify-between gap-4">
         <div>
           <h1 className="text-primary text-lg md:text-xl font-semibold">Events</h1>
           <p className="text-gray-700 text-[11px] md:text-xs mt-0.5 md:mt-1">
